928660f6e2c7741560d6111d7558751cece0e0c8
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-01-08 09:42:14 -0700

    building base

---
+++ contracts/FundMe.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.9 <0.9.0;
+
+// import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+// import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+// import "asdasd";
+import "interfaces/IHederaTokenService.sol";
+
+contract FundMe {
+    // using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    uint256 public price;
+
+    constructor(uint256 _price) public {
+        price = _price;
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        uint256 minimumUSD = 50 * 10**18;
+        require(msg.value >= price, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        uint256 minimumUSD = 50 * 10**18;
+        // uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (minimumUSD * precision) / price;
+    }
+    cryptoTransfer(TokenTransferList[] calldata tokenTransfers)
+    function withdraw() public payable onlyOwner {
+        msg.sender.transfer(address(this).balance);
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,64 @@
+const { Client, ContractCreateTransaction, FileCreateTransaction, FileId, Hbar, PrivateKey, ContractCallQuery, ContractFunctionParameters, ContractExecuteTransaction } = require("@hashgraph/sdk");
+require("dotenv").config();
+const json = require('./compiled.json');
+
+async function main () {
+
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = process.env.MY_PRIVATE_KEY;
+
+    if (myAccountId == null ||
+        myPrivateKey == null ) {
+        throw new Error("Environment variables myAccountId and myPrivateKey must be present");
+    }
+
+    const client = Client.forPreviewnet();
+
+    client.setOperator(myAccountId, myPrivateKey)
+
+    const compiled = json['data']['bytecode']['object'];
+    // Store Contact in file service. Different from eth. Transaction size is smaller on hedera for security 
+    const mycontract = await new FileCreateTransaction()
+        .setContents(compiled)
+        .setKeys([PrivateKey.fromString(myPrivateKey)])
+        // The default max fee of 1 HBAR is not enough to make a file ( starts around 1.1 HBAR )
+        .setMaxTransactionFee(new Hbar(2)) // 2 HBAR
+        .execute(client);
+    
+    const TransactionReceipt  = await mycontract.getReceipt(client);
+    const fileid =  new FileId(TransactionReceipt.fileId);
+    console.log("file ID: " + fileid);
+    // Deploy Contract
+    const deploy = await new ContractCreateTransaction()
+        .setGas(300)
+        .setBytecodeFileId(fileid)
+        .execute(client);
+
+    const receipt = await deploy.getReceipt(client); //Get the new contract 
+    const newContractId = receipt.contractId;        
+    console.log("The contract ID is " + newContractId);
+
+    const setter = await new ContractExecuteTransaction()
+        .setContractId(newContractId)
+        .setGas(400000)
+        .setFunction("set", new ContractFunctionParameters().addUint256(7))
+        .setMaxTransactionFee(new Hbar(3))
+
+    // see input protobuff being sent by this ContractExecuteTransaction
+    // console.log(JSON.stringify(setter._makeTransactionBody()))
+    const contractCallResult = await setter.execute(client);
+    const testing = await contractCallResult.getReceipt(client);
+    console.log("Status Code:", testing.status)
+    //console.log(JSON.stringify(testing))
+
+    const getter = await new ContractCallQuery() // 
+        .setContractId(newContractId)
+        .setFunction("get")
+        .setGas(300000)
+        .setMaxQueryPayment(new Hbar(1)) // defaults to 1, if requires more than one need change
+
+    const contractGetter = await getter.execute(client);
+    const message = await contractGetter.getUint256(0);
+    console.log("contract message: " + message);
+}
+main();
